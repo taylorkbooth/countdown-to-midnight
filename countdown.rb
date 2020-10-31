69db9675ca5f3efb96e602d15530cd5009dc2be3
Taylor Booth <taylorkbooth@gmail.com>
2020-10-30 21:42:58 -0400

    commit tree 42c52135b382fae5464da4b7fe00a8b5a4d21e78

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
 
-def countdown
+def countdown(seconds)
+    # counter = 10
+
+    while seconds > 0
+       puts "#{seconds} SECOND(S)!"
+       seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    # counter = 5
+    while seconds > 0
+        sleep(1)
+        seconds -= 1
+    end
 end
